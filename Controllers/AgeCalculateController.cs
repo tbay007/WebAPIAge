63bac59c8989dc3f4c958dd3a7f6d8fdda48c93e
Travis Bay <travisbay84@gmail.com>
2024-06-24 11:10:34 -0400

    Added comments

--- Controllers/AgeCalculateController.cs
+++ Controllers/AgeCalculateController.cs
@@ -23,23 +23,25 @@ public class AgeCalculateController : ControllerBase
     [HttpPost(Name = "AgeCalculate")]
     public string AgeCalculation(string birthday)
     {
+        //Checks for an empty string, but swagger enforces the parameter
         if (String.IsNullOrEmpty(birthday))
         {
+            //Throws an error saying the birthday field was empty
             throw new Exception("Birthday is empty!  Fix it!");
         }
+
         DateTime birthdayDate;
-        bool isDate = DateTime.TryParse(birthday, out birthdayDate);
+        bool isDate = DateTime.TryParse(birthday, out birthdayDate); //tries to parse a date
         if (isDate)
         {
             TimeSpan birthdayDiff = DateTime.Now.Subtract(birthdayDate);
-
-
             int daysOld = birthdayDiff.Days;
-            int yearsOld = daysOld / 365;
+            int yearsOld = daysOld / 365; //divides by 365 for how many days
             return yearsOld.ToString();
         }
         else{
-            _logger.LogError("Birthday was not formatted correctly");
+            //Logs that the birthday was not formatted correctly and returns the string
+            _logger.LogError("Birthday was not formatted correctly: " + birthday);
             return "birthday was not formatted correctly";
         }
     }
